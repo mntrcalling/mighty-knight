69ddcb7f24c7032b89866cf107394838f18a8b68
Janak <mntrcalling@hotmail.com>
2024-06-09 13:02:10 -0500

    Create Operator.py

---
+++ Day 2/Operator.py
@@ -0,0 +1,11 @@
+x = 6
+y = 2
+z = 7
+print(f"{x} plus {y} equals {x + y}")
+print(f"{x} minus {y} equals {x - y}")
+print(f"{x} times {y} equals {x * y}")
+print(f"{x} divided by {y} equals {x / y}")
+
+print(f"{z} divided to the floor of {y} equals {z // y}")
+print(f"{z} modular {y} equals {z % y}")
+print(f"{x} to the power of {y} equals {x**y}")
