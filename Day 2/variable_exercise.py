e621d51340f223714f3dd926324db161846412bc
Janak <mntrcalling@hotmail.com>
2024-06-11 22:27:40 -0500

    Create variable_exercise.py

---
+++ Day 2/variable_exercise.py
@@ -0,0 +1,6 @@
+#Variables Practice #3
+#Declare the variable course, assign it the value "Python", and print the following sentence:
+#You are taking a course course
+
+course = "Python"
+print("You are taking a" + " " + course + " " + "course")
